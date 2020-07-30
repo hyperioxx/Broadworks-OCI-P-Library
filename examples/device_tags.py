d3a63584f3f81556b88174e3c34dc99e9473ceb6
Aaron Parfitt <aaronparfitt123@gmail.com>
2020-07-30 09:22:34 +0100

    added example

---
+++ examples/device_tags.py
@@ -0,0 +1,30 @@
+from BroadworksOCIP import Client
+from BroadworksOCIP.broadworks.schema.request.group import GroupDeviceTypeCustomTagGetListRequest, GroupDeviceTypeCustomTagAddRequest, GroupDeviceTypeCustomTagDeleteListRequest
+
+client = Client(username="username", password="password", address="https://api.broadsoft.com/webservice/services/ProvisioningService")
+
+client.login() 
+
+
+x1 = GroupDeviceTypeCustomTagGetListRequest("serviceproviderID", "GroupID", "Polycom VVX 600")
+response = client.send(x1)
+print(response.get_tags())
+
+
+x2 = GroupDeviceTypeCustomTagAddRequest('serviceproviderID', 'GroupID', "Polycom VVX 600", '%ffdffd%', "TEST")
+
+response = client.send(x2)
+print(response)
+
+x4 = GroupDeviceTypeCustomTagGetListRequest("serviceproviderID", "GroupID", "Polycom VVX 600")
+response = client.send(x4)
+
+print(response.get_tags())
+
+
+
+x3 = GroupDeviceTypeCustomTagDeleteListRequest('serviceproviderID', 'GroupID', "Polycom VVX 600", '%ffdffd%')
+
+
+response = client.send(x3)
+print(response)
